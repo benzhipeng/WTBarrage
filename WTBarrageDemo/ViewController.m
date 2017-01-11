7fb90214070ab8a456eb3d692e9267432bd825bb
ben <ben@bendeMacBook-Pro.local>
2017-01-11 14:28:57 +0800

    完善demo

--- WTBarrageDemo/ViewController.m
+++ WTBarrageDemo/ViewController.m
@@ -38,33 +38,27 @@
         make.height.mas_equalTo(110);
         make.centerY.mas_equalTo(self.view.mas_centerY);
     }];
- 
-
-}
-
-- (IBAction)sendBarrage:(id)sender {
     
-    WTBarrageContent* barrage = [[WTBarrageContent alloc] init];
-    barrage.avatar = @"http://h.hiphotos.bdimg.com/album/h%3D370%3Bq%3D90/sign=95b22d5bbb12c8fcabf3f0cacc38e378/6159252dd42a2834d3a2697059b5c9ea15cebf41.jpg";
-    barrage.avatarPlaceHolder = nil;
-    barrage.content = @"离岸人民币隔夜存款利率飙升至纪录高点80%";
-    [self.barrageContainer insertBarrages:@[barrage]];
-//  
-//    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(refresh) userInfo:nil repeats:YES];
- 
-//    [self.barrageContainer insertCustomFormat:@{@"text":@"frank"}];
-
+     self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(refresh) userInfo:nil repeats:YES];
 }
+
 
 - (void)refresh{
 
     
-//    NSString* barrage = @"离岸人民币隔夜存款利率飙升至纪录高点80%";
-//    NSMutableArray* array = [NSMutableArray arrayWithCapacity:0];
-//    for(NSInteger i = 0;i < 10; i++){
-//        NSString* tmp = [barrage substringToIndex:arc4random() % (barrage.length)];
-//        [array addObject:tmp];
-//    }
+    NSString* barrage = @"离岸人民币隔夜存款利率飙升至纪录高点80%";
+    NSMutableArray* array = [NSMutableArray arrayWithCapacity:0];
+    for(NSInteger i = 0;i < 10; i++){
+        
+        NSString* tmp = [barrage substringToIndex:arc4random() % (barrage.length)];
+        WTBarrageContent* barrage = [[WTBarrageContent alloc] init];
+        barrage.avatar = @"http://h.hiphotos.bdimg.com/album/h%3D370%3Bq%3D90/sign=95b22d5bbb12c8fcabf3f0cacc38e378/6159252dd42a2834d3a2697059b5c9ea15cebf41.jpg";
+        barrage.avatarPlaceHolder = [UIImage imageNamed:@"avatar"];
+        barrage.trailImage = [UIImage imageNamed:@"fengche"];
+        barrage.content = tmp;
+        [array addObject:barrage];
+    }
+    [self.barrageContainer insertBarrages:array];
    
     
 //    UIView* view =  [self.barrageContainer.subviews lastObject];
